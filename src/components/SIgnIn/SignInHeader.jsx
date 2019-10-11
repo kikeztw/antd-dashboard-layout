1e91ecc2e7595ed93a6438afdea3041b9255a43e
carlos <kikeztw@gmail.com>
2019-10-11 19:40:32 -0400

    fixed css names

--- src/components/SIgnIn/SignInHeader.jsx
+++ src/components/SIgnIn/SignInHeader.jsx
@@ -4,7 +4,7 @@ import { Typography } from 'antd';
 const { Title } = Typography;
 
 export default () => (
-  <div className="sing-in-form-header">
+  <div className="form-header sing-in-header">
     <img src={require('../../assest/images/antLogo.svg')} alt="logo" className="form-header-logo" />
     <Title level={4}>Antd Layout DashBoard</Title>
   </div>
--- src/components/SignUp/SignUpHeader.jsx
+++ src/components/SignUp/SignUpHeader.jsx
@@ -4,7 +4,7 @@ import { Typography } from 'antd';
 const { Title } = Typography;
 
 export default () => (
-  <div className="sing-up-form-header">
+  <div className="form-header sing-up-header">
     <img src={require('../../assest/images/antLogo.svg')} alt="logo" className="form-header-logo" />
     <Title level={4}>Create a account</Title>
   </div>
